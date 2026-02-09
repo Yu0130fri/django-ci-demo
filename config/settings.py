3b2ccf7ac0d5b9d619ddf1163ebd0cb5299e1eb8
Yu0130fri <suugakunohito@gmail.com>
2026-02-09 20:10:33 +0900

    fix allowed_hosts

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-i83#9_#zndb0(i0=@8eg*py_tph6cpd9yu70b47ma&48cp)md#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS: list[str] = []
+ALLOWED_HOSTS: list[str] = [h for h in os.getenv("ALLOWED_HOSTS", "").split(",") if h]
 
 
 # Application definition
